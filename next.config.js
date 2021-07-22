b3d26b6842a0c40d006b4dac9df1bb4af24b2f5d
Sander Cokart <cokart32@gmail.com>
2021-07-22 20:56:27 +0200

    added redirects

--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
+const path = require('path')
+
 module.exports = {
   reactStrictMode: true,
-}
+  async redirects () {
+    return [
+      { source: '/', destination: '/login', permanent: true }
+    ];
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  env: {
+    API_URL: 'http://localhost:8000/',
+  }
+};
